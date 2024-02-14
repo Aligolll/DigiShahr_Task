dfbc45f131766ff7cc79d020e953f0fd448c68ea
AliGolll <109023504+Aligolll@users.noreply.github.com>
2024-02-14 17:33:32 +0330

    Delete SQL-1-1.sql

--- SQL-1-1.sql
+++
@@ -1,5 +0,0 @@
-select userId , sum(sales) net from orders
-where userid in (select id from users 
-        where registeredDate > date('2022-12-31' , '+1 day' ))  
-group by userid
-order by net desc limit 10
